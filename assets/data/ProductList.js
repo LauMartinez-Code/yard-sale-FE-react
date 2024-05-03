70ce6c290b91b26d607d77863278d47d2cdf61f0
Lautaro Martinez <laumartinez.exe@gmail.com>
2024-05-03 20:35:06 -0300

    El <template> "product-card" se cambia al web component ProductCard.js. El array de datos "productList" de index.html se mueve a un archivos .js aparte.

---
+++ assets/data/ProductList.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        title: 'Audio cassette',
+        price: 50,
+        coverSrc: './assets/product-covers/audio-cassette.jpg',
+        isInCart: false,
+    },
+    {
+        title: 'Brownsville NY electric guitar - Black and white original',
+        price: 700,
+        coverSrc: './assets/product-covers/electric-guitar.jpg',
+        isInCart: true,
+    },
+    {
+        title: 'Pentax camera - Black and grey',
+        price: 900,
+        coverSrc: './assets/product-covers/camera.jpg',
+        isInCart: false,
+    },
+    {
+        title: 'Wireless PC mouse Logitech',
+        price: 350,
+        coverSrc: './assets/product-covers/pc-mouse.jpg',
+        isInCart: false,
+    },
+    {
+        title: 'Rolex wrist watch',
+        price: 1200,
+        coverSrc: './assets/product-covers/wrist-watch.jpg',
+        isInCart: true,
+    },
+    {
+        title: 'Brown wooden chair',
+        price: 100,
+        coverSrc: './assets/product-covers/chair.jpg',
+        isInCart: false,
+    },
+    {
+        title: 'Gigabyte gamer speaker',
+        price: 630,
+        coverSrc: './assets/product-covers/speaker.jpg',
+        isInCart: true,
+    },
+    {
+        title: 'Silver desktop microphone',
+        price: 250,
+        coverSrc: './assets/product-covers/microphone.jpg',
+        isInCart: false,
+    },
+];
