19b918c743c49fb91f77a2355d5575cef8bc2b6d
Lautaro Martinez <laumartinez.exe@gmail.com>
2025-02-02 19:19:26 -0300

    Refactor routing to use RouterProvider and move routes to a dedicated router file. New React Router setting up for using Data APIs

---
+++ src/router.jsx
@@ -0,0 +1,35 @@
+
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
+import AuthLayout from '@/layouts/AuthLayout.jsx';
+import Home from '@/pages/Home/Home.jsx';
+import Account from '@/pages/Account/Account.jsx';
+import Orders from '@/pages/Orders/Orders.jsx';
+import Login from '@/pages/Auth/Login/Login.jsx';
+import CreateAccount from '@/pages/Auth/CreateAccount/CreateAccount.jsx';
+import PasswordRecovery from '@/pages/Auth/PasswordRecovery/PasswordRecovery.jsx';
+import PassRecoveryEmailSent from '@/pages/Auth/PasswordRecovery/PassRecoveryEmailSent/PassRecoveryEmailSent.jsx';
+import CreatePassword from '@/pages/Auth/CreatePassword/CreatePassword.jsx';
+import PageNotFound from '@/pages/error/PageNotFound/PageNotFound.jsx';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/'>
+            <Route index element={<Home />}/>
+            <Route path='account' element={<Account />} />
+            <Route path='orders' element={<Orders />} >
+                <Route path=':orderId' element={<h1>Order Details</h1>} />
+            </Route>
+            <Route element={<AuthLayout />}>
+                <Route path='login' element={<Login />} />
+                <Route path='create-account' element={<CreateAccount />} />
+                <Route path='password-recovery' element={<PasswordRecovery />} >
+                    <Route path='email-sent' element={<PassRecoveryEmailSent />} />
+                </Route>
+                <Route path='create-password' element={<CreatePassword />} />
+            </Route>
+            <Route path='*' element={<PageNotFound />} />
+        </Route>
+    )
+);
+
+export default router;
