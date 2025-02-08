5dea4f7018020d319b585a099eec29d624e7eedf
Lautaro Martinez <laumartinez.exe@gmail.com>
2025-02-08 15:43:30 -0300

    Implement CreatePassword component

--- src/pages/Auth/CreatePassword/CreatePassword.jsx
+++ src/pages/Auth/CreatePassword/CreatePassword.jsx
@@ -1,9 +1,39 @@
-// import React from 'react'
+import { useNavigate } from "react-router";
 
 const CreatePassword = () => {
-  return (
-    <h1>Create Password</h1>
-  )
+    const navigate = useNavigate();
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        navigate('/login');
+    }
+
+    return (<>
+        <h1 className="title-h1">Create a new password</h1>
+        <h4 className="subtitle">Enter a new password for your account</h4>
+
+        <form className="form-vertical" onSubmit={handleFormSubmit}>
+            <label className="form-vertical__label">
+                Password
+                <input type="password" className="form-vertical__input" placeholder="Enter password"
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" maxLength="80" required />
+                <small className="form-vertical__text-small">
+                    It must contain 8 or more characters, in which there is at least one number and one upper and lower case letter
+                </small>
+            </label>
+            <label className="form-vertical__label">
+                Re-enter password
+                <input type="password" className="form-vertical__input" placeholder="Re-enter password"
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" maxLength="80" required />
+                <span className="form-vertical__invalid-feedback">
+                    Enter a valid password
+                </span>
+            </label>
+            <button type="submit" className="btn btn--primary">
+                Confirm
+            </button>
+        </form>
+    </>)
 }
 
-export default CreatePassword
+export default CreatePassword;
